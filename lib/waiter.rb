e2ce737f22e8c325b0aca71786de9436ea1a5205
Theodore Schafer <theodore.schafer@gmail.com>
2020-09-17 15:41:59 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -24,6 +24,15 @@ class Waiter
   end
 
   def best_tipper
+    best_tip = 0
+    best_tipper = nil
+    meals.each do |customer|
+      if customer.tip > best_tip
+        best_tip = customer.tip
+        best_tipper = customer
+      end
+    end
+    best_tipper
 
   end
 
