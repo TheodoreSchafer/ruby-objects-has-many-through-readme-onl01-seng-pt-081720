13870753d605d22a5057c69a75047a89959584e7
Theodore Schafer <theodore.schafer@gmail.com>
2020-09-17 14:54:09 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -2,11 +2,14 @@ class Meal
 
   attr_accessor :waiter, :customer, :total, :tip
 
+  @@all - []
+
   def initialize
     @waiter = waiter
     @customer = customer
     @total = total
     @tip = tip
+    @@all << self
   end
 
 end
