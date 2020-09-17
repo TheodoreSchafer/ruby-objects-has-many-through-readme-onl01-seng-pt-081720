c69b8b627110d9e4dfba76dfdf67153d0f6d5ca5
Theodore Schafer <theodore.schafer@gmail.com>
2020-09-17 14:51:46 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,9 +1,12 @@
 class Waiter
   attr_accessor :name, :experience
 
+  @@all = []
+
   def initialize(name, experience)
     @name = name
     @experience = experience
+    @@all << self
   end
 
 end
